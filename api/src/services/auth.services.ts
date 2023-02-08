a1869dd5c5864f0aa98e29026ea91a5e7edfdfad
Fábio Eduardo Circuncisao <fabio.eduardo@crmservices.com.br>
2023-02-08 02:12:43 -0300

    Auth jwt

---
+++ api/src/services/auth.services.ts
@@ -0,0 +1,28 @@
+import { IrrobaApi } from "./api";
+import {ErrorHandlerFunction} from "./helpers.services"
+
+export const getToken = async({
+  email,
+  password,
+}:{
+  email: string;
+  password: string;
+}) => {
+  const response = await IrrobaApi.post<{
+    refresh: string;
+    access: string
+  }>(
+    "/getToken",
+    {email,password},
+    {
+      headers: {"Content-Type": "application/json" }
+    }
+  ).catch(ErrorHandlerFunction);
+  if(response) {
+    const data = {
+      data: response.data,
+      status: response.status
+    };
+    return data;
+  }
+}
