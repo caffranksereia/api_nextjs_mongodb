ef6f098cb19516632fb6880240f1b8b73bb60bcc
Fábio Eduardo Circuncisao <fabio.eduardo@crmservices.com.br>
2023-02-08 02:13:35 -0300

    product services return info product

---
+++ api/src/services/product.services.ts
@@ -0,0 +1,26 @@
+import { IrrobaApi } from "./api";
+import { Product } from "../interfaces/product.interfaces";
+import {ErrorHandlerFunction} from "./helpers.services";
+
+export type infoData ={
+  id: string
+  token: string
+}
+
+export const getProduct = async({
+  token,
+  id
+}:infoData) => {
+  const response = await IrrobaApi.get<Product>(
+    `product/${id}`,
+    {
+      headers: {"Authorization":token}
+    }
+  ).catch(ErrorHandlerFunction);
+  if(response){
+    const data = {data: response.data, status: response.statu};
+    return data
+  }
+}
+
+
