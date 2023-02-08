981077be99a901375b5c2d62de61ba4d2d16b9da
Fábio Eduardo Circuncisao <fabio.eduardo@crmservices.com.br>
2023-02-08 02:10:00 -0300

    style button

---
+++ api/src/components/Button/styles.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const styledButton = styled.button`
+font-size: 14px;
+  font-style: normal;
+  width: 100%;
+  height: 50px;
+  padding: 0;
+  text-transform: uppercase;
+  margin-top: 1em;
+  margin-bottom: 1em;
+  text-align: center;
+  border-radius: 0.5em;
+  border: none;
+  color: #fff;
+  justify-content: space-between;
+`
